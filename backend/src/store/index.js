ed1c85b8a9b1f1d4a989b111073cb5b0cd239c60
junior <antonioivo.3@gmail.com>
2024-03-07 09:48:45 -0300

    ⛳:init vue: tailwind,vuex,router

---
+++ backend/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state:{
+        teste:'123'
+    },
+    getters: {},
+    actions:{},
+    mutations:{}
+})
+
+
+export default store;
